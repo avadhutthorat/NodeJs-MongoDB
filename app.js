85385c313e512b5757cc927335f09643ecd98a7f
Avadhut Thorat <avadhutthorat40@gmail.com>
2019-12-29 14:03:31 +0530

    Mongoose - Adding and using User model

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 const errorController = require("./controllers/error");
 
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "pug"); // configuring pug for express
@@ -15,15 +17,15 @@ app.set("views", "views"); // will look for view in views folder
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use((req, res, next) => {
-//   // User.findUserById("5e06f69769459979f428c9a8")
-//   //   .then(user => {
-//   //     req.user = new User(user.name, user.email, user.cart, user._id);
-//   //     next();
-//   //   })
-//   //   .catch(err => console.log(err));
 
-// });
+app.use((req, res, next) => {
+  User.findById("5e08644eb95cd76a64cc2ced")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
@@ -35,7 +37,19 @@ mongoose
     "mongodb+srv://avadhut:fMyI2X3KLZVx43IR@cluster0-wnwz9.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then(result => {
-    console.log("connected to database");
+    User.findOne().then(foundUser => {
+      if (!foundUser) {
+        const user = new User({
+          name: "Avadhut",
+          email: "avadhut@test.com",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
   })
   .catch(() => console.log(`Not connected to database`));
